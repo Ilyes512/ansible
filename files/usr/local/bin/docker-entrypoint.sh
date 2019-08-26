f2bd7bff430019e8b123e12b0939a7cef5c84dd5
Ilyes512 <ilyes.ahidar@gmail.com>
2019-08-26 10:19:30 +0200

    Removed more unnecessary curly braces

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-if [ !  -z "${KUBECONFIG_OVERRIDE:-}" ]; then
+if [ !  -z "$KUBECONFIG_OVERRIDE" ]; then
     mkdir -p /${HOME:-root}/.kube
-    printf "%s" "${KUBECONFIG_OVERRIDE}" > /${HOME:-root}/.kube/config-override
+    printf "%s" "$KUBECONFIG_OVERRIDE" > /${HOME:-root}/.kube/config-override
     export KUBECONFIG="/${HOME:-root}/.kube/config-override"
 fi
 
